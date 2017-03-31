5082f18f3b1fe6d298cb0023131d9099d86394a9
ehguo <edward.h.guo@gmail.com>
2017-03-31 15:13:33 -0700

    finished rps

--- rps/main.js
+++ rps/main.js
@@ -1,37 +1,37 @@
+function handleClick(event) {
+  const player = +event.target.id;
+  const computer = throwHand();
+
+  const winner = determineWinner(player, computer);
+  updateDOM(player, computer, winner);
+}
+
 function throwHand() {
   return Math.floor(Math.random() * 3);
 }
 
-function handleClick(event) {
-  const playerThrow = event.target.textContent;
-  console.log('Player threw a ' + playerThrow);
-  const computerThrow = throwHand();
-  if (computerThrow === 1) {
-    console.log('AI threw a Rock');
-    if (playerThrow === 'Rock') {
-      console.log('Tie');
-    } else if (playerThrow === 'Paper') {
-      console.log('Player wins');
-    } else {
-      console.log('AI wins');
-    }
-  } else if (computerThrow === 2) {
-    console.log('AI threw a Paper');
-    if (playerThrow === 'Rock') {
-      console.log('AI wins');
-    } else if (playerThrow === 'Paper') {
-      console.log('Tie');
-    } else {
-      console.log('Player wins');
-    }
+function determineWinner(p1, p2) {
+  return p1 === p2
+    ? 'tie'
+    : p1 + 2 === p2
+      ? 'player'
+      : p2 + 2 === p1
+        ? 'computer'
+        : p1 > p2
+          ? 'player'
+          : 'computer';
+}
+
+function updateDOM(player, computer, winner) {
+  document.getElementById(player).style.border = '2px solid red';
+  document.getElementById('c' + computer).style.border = '2px solid red';
+
+  const loser = winner === 'player' ? 'computer' : 'player';
+  if (winner !== 'tie') {
+    document.getElementById(winner).textContent = 'Winner Winner Winner';
+    document.getElementById(loser).textContent = 'Loser Loser Loser';
   } else {
-    console.log('AI threw a Scissors');
-    if (playerThrow === 'Rock') {
-      console.log('Player wins');
-    } else if (playerThrow === 'Paper') {
-      console.log('AI wins');
-    } else {
-      console.log('Tie');
-    }
+    document.getElementById('player').textContent = 'Tie Tie Tie';
+    document.getElementById('computer').textContent = 'Tie Tie Tie';
   }
 }
