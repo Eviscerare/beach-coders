0f2cda31ff4b1616b17e62ae31d55d630921bfe9
Edward Guo <edward.h.guo@gmail.com>
2017-04-12 20:29:38 -0700

    jvfloat

--- multipageform/scripts/main.js
+++ multipageform/scripts/main.js
@@ -10,10 +10,20 @@ $().ready(() => {
   }
 
   function finish(e) {
+    console.log(e);
     e.preventDefault();
     $(this).toggle();
     const url = document.URL;
+    // function nvpairsplitter() {
+    //   const url = document.URL;
+    //   const nvstrings = url.split('?')[1].split('&');
+    //   const nvobj = {};
+    //   arr.forEach()
+
+    //   return result;
+    // }
     const name = url.split('?')[1].split('=')[1];
+    // const food = $('.second input').value();
     const food = e.target[0].value;
     displayResult(name, food);
   }
